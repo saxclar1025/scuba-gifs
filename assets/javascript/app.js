52a54ac9637d2c72e7ac158c6390bb626a7fdb30
Samuel Falcon <saxclar1025@gmail.com>
2018-03-07 09:26:45 -0500

    created JS framework and commented sample Giphy API AJAX request URL

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+//sample AJAX request url
+//https://api.giphy.com/v1/gifs/search?api_key=MPSSfVkzoXCE4jvCDJ7UJutlRqMKk8yt&q=searchTerm&limit=numberOfResults&offset=0&rating=G&lang=en
+$(document.body).ready(function() {
+
+});
